2fb6750a6c063a9591bda7e84505b58c2f17bd50
Phillip Manley <phillip.manley@helmholtz-berlin.de>
2020-06-09 14:56:09 +0200

    fixed checking for required keys in input dict

--- src/refractive_index_database/jcm_interface.py
+++ src/refractive_index_database/jcm_interface.py
@@ -3,14 +3,14 @@ import sys
 import os
 
 def get_permittivity(inputs):
-    
-    
+
+
     if not isinstance(inputs, dict):
         raise ValueError("this function requires a dicionary input")
     required_keys = {'path', 'name', 'EMOmega'}
-    if not all(key in inputs for key in required_keys):
-        raise ValueError("missing one of the following" +
-                         "keys in input dict {}".format(required_keys))
+    for required_key in required_keys:
+        if required_key not in inputs:
+            raise ValueError("missing {} in input dict".format(required_key))
     if not isinstance(inputs['name'], str):
         raise ValueError("input {} must be of type {}".format("name", str))
     if not isinstance(inputs['path'], str):
@@ -20,13 +20,13 @@ def get_permittivity(inputs):
         raise ValueError("input {} must be" +
                          " of types {}".format("EMOmega",
                                                {float, complex}))
-    
+
     dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
     print("adding path :<{}> to PATH".format(dir_path))
-    sys.path.insert(0, dir_path)    
+    sys.path.insert(0, dir_path)
     from refractive_index_database.config import get_config
     from refractive_index_database.material_database import MaterialDatabase
-        
+
     config = get_config()
     config['Path'] = inputs['path']
     mdb = MaterialDatabase(config=config)
