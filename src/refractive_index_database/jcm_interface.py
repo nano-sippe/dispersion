4e8062c5c935d2fdad62240cd6dc892c3bd02f54
Phillip Manley <manley@zib.de>
2020-06-09 13:18:57 +0200

    fixed format of jcm_interface

--- src/refractive_index_database/jcm_interface.py
+++ src/refractive_index_database/jcm_interface.py
@@ -20,18 +20,16 @@ def get_permittivity(inputs):
         raise ValueError("input {} must be" +
                          " of types {}".format("EMOmega",
                                                {float, complex}))
-    
-    dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-    print("adding path :<{}> to PATH".format(dir_path))
-    sys.path.insert(0, dir_path)    
+
     from refractive_index_database.config import get_config
     from refractive_index_database.material_database import MaterialDatabase
-        
+
     config = get_config()
     config['Path'] = inputs['path']
     mdb = MaterialDatabase(config=config)
     mat = mdb.get_material(inputs['name'])
-    complex_eps = mat.get_permittivity(inputs['EMOmega'],
+    omega = real(inputs['EMOmega']).reshape(1,)
+    complex_eps = mat.get_permittivity(omega,
                                spectrum_type='angularfrequency',
                                unit='1/s')
-    return (real(complex_eps),imag(complex_eps))
+    return eye(3,3)*complex_eps
